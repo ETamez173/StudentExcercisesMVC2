dc2ee32418c41e089ee128d1f4395bfddd6b1bab
ETamez9173 <elitamez17@gmail.com>
2020-07-14 20:13:12 -0500

    added exercise model and exercise edit view

---
+++ StudentExcercisesMVC2/Controllers/ExercisesController.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using StudentExcercisesMVC2.Models;
+using StudentExcercisesMVC2.Models.ViewModels;
+
+namespace StudentExcercisesMVC2.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ExercisesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+
+        // GET: ExercisesController
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name, Language FROM Exercise";
+
+                    var reader = cmd.ExecuteReader();
+                    var exercises = new List<Exercise>();
+
+                    while (reader.Read())
+                    {
+                        var exercise = new Exercise()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Language = reader.GetString(reader.GetOrdinal("Language")),
+                        };
+
+                        exercises.Add(exercise);
+
+                    }
+                    reader.Close();
+
+                    return View(exercises);
+                }
+            }
+        }
+
+        // GET: ExercisesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var exercise = GetExerciseById(id);
+            return View(exercise);
+        }
+
+        // GET: ExercisesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ExercisesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ExercisesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var exercise = GetExerciseById(id);
+            var viewModel = new ExerciseEditViewModel()
+            {
+
+                ExerciseId = exercise.Id,
+                Name = exercise.Name,
+                Language = exercise.Language,
+
+
+            };
+
+
+            return View(viewModel);
+        }
+
+        // POST: ExercisesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ExerciseEditViewModel exercise)
+        {
+            try
+            {
+
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Exercise 
+                                           SET Name = @name,
+                                               Language = @language
+                                           
+                                            WHERE Id = @id";
+
+                        cmd.Parameters.Add(new SqlParameter("@name", exercise.Name));
+                        cmd.Parameters.Add(new SqlParameter("@language", exercise.Language));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        var rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected < 1)
+                        {
+                            return NotFound();
+
+                        }
+
+                    }
+
+                }
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: ExercisesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ExercisesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private Exercise GetExerciseById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name, Language FROM Exercise WHERE Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    var reader = cmd.ExecuteReader();
+                    Exercise exercise = null;
+
+                    while (reader.Read())
+                    {
+                        exercise = new Exercise()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Language = reader.GetString(reader.GetOrdinal("Language")),
+                            
+
+                        };
+
+                    }
+                    reader.Close();
+                    return exercise;
+                }
+            }
+        }
+    }
+}
---
+++ StudentExcercisesMVC2/Models/ViewModels/ExerciseEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExcercisesMVC2.Models.ViewModels
+{
+    public class ExerciseEditViewModel
+    {
+
+
+        public int ExerciseId { get; set; }
+
+        [Display(Name = "Exercise")]
+        [Required(ErrorMessage = "Exercise Name is required!")]
+        [MinLength(2, ErrorMessage = "Exercise Name should be at least 2 characters long")]
+        public string Name { get; set; }
+
+        [Display(Name = "Language")]
+        [Required(ErrorMessage = "Language is required!")]
+        [MinLength(2, ErrorMessage = "Language should be at least 2 characters long")]
+        public string Language { get; set; }
+
+
+    }
+}
